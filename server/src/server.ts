ee3d58a81ae601ba4c51f813b37a905ce5fe92ea
Hilderlan <hilakt9698@gmail.com>
2020-09-15 10:22:36 -0300

    Route to create a class made.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
-  console.log('Hello world!');
-  response.send('LOL');
-});
+app.use(routes);
 
 app.listen(5000, () => {
   console.log('Server started!');
